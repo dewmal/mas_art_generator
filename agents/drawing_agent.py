1fb4fe8c367c866df8efa24cc1199917e005fe14
Dewmal <dewmal@syigen.com>
2021-12-30 08:16:22 +0530

    Draw image and show output

--- agents/drawing_agent.py
+++ agents/drawing_agent.py
@@ -4,6 +4,9 @@ from agents import Agent
 import random
 import uuid
 from PIL import Image, ImageDraw
+from sewar.full_ref import uqi
+import numpy as np
+import matplotlib.pyplot as plt
 
 log = logging.getLogger(Agent.DrawingAgent)
 
@@ -24,17 +27,39 @@ class DrawingAgent:
         log.info("Agent AgentOne Stopping...")
 
     async def execute(self, *args, **kwargs):
+        image_width = 1600
+        image_height = 1600
+        pixel_size = 8
+
+        original_image = Image.open('./data/sample_1.jpg').resize((image_width, image_height))
+        original_image = np.array(original_image)
+
+        run_id, gen_image = await self.draw_image(image_width, image_height, pixel_size)
+        gen_image = np.array(gen_image)
+
+        log.info(f"image shape: {original_image.shape} {gen_image.shape}")
+
+        uqi_score = uqi(gen_image, original_image)
+        log.info(f"{run_id} MSE score: {uqi_score}")
+
+        plt.imshow(gen_image)
+
+        f = plt.figure()
+        f.add_subplot(1, 2, 1)
+        plt.imshow(original_image)
+        f.add_subplot(1, 2, 2)
+        plt.imshow(gen_image)
+        plt.show()
+
+    async def draw_image(self, image_width, image_height, pixel_size):
         run_id = uuid.uuid1()
 
         log.info(f'Processing run_id: {run_id}')
 
-        image_width = 2000
-        image_height = 2000
-
         image = Image.new('RGB', (image_width, image_height))
 
-        rectangle_width = 100
-        rectangle_height = 100
+        rectangle_width = pixel_size
+        rectangle_height = pixel_size
 
         number_of_squares = int(image_width * image_height / rectangle_width / rectangle_height)
 
@@ -49,8 +74,8 @@ class DrawingAgent:
         y_points = [i for i in range(int(image_height // rectangle_height))]
 
         while len(all_points) != number_of_squares:
-            rectangle_x = random.choice(x_points)*rectangle_width
-            rectangle_y = random.choice(y_points)*rectangle_height
+            rectangle_x = random.choice(x_points) * rectangle_width
+            rectangle_y = random.choice(y_points) * rectangle_height
             rectangle_red = random.randint(0, 255)
             rectangle_green = random.randint(0, 255)
             rectangle_blue = random.randint(0, 255)
@@ -74,5 +99,4 @@ class DrawingAgent:
                         rectangle_blue)
                 )
             iteration += 1
-        # image.save(f'./output/{run_id}.png')
-        image.show()
+        return run_id, image
